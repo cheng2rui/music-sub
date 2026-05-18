dc3b750908a1b71d740a5f3f684cb3c198aa07e6
rey <rey@cheng.com>
2026-05-18 20:18:00 +0800

    chore: release v0.7.3

--- app/version.py
+++ app/version.py
@@ -1,3 +1,3 @@
 """Application version."""
 
-APP_VERSION = "0.7.2"
+APP_VERSION = "0.7.3"
--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -58,7 +58,7 @@ function handleLogout() {
           <span>已登录</span>
           <button @click="handleLogout" class="btn-logout">退出</button>
         </div>
-        <div class="version-tag">v0.7.2</div>
+        <div class="version-tag">v0.7.3</div>
       </div>
     </aside>
 
