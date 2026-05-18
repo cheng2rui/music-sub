e57b4dc959ae03b6df1aa31e62b850d08dd27c83
rey <rey@cheng.com>
2026-05-18 19:41:15 +0800

    chore: release v0.7.2

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -52,7 +52,7 @@ function handleLogout() {
           <span>已登录</span>
           <button @click="handleLogout" class="btn-logout">退出</button>
         </div>
-        <div class="version-tag">v0.7.1</div>
+        <div class="version-tag">v0.7.2</div>
       </div>
     </aside>
 
